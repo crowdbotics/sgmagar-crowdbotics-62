84b37e4c3ff2c6e6563d01950bca1b1a5c700537
Saurav <sp.gharti@gmail.com>
2018-03-03 18:51:17 +0000

    add package django-twitterflux

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-twitterflux', 'url': 'http://pypi.python.org/pypi/django-twitterflux/0.0.3'},
 	{'name':'django-twitter', 'url': 'http://pypi.python.org/pypi/django-twitter/0.1.0'},
 	{'name':'django-paypal', 'url': 'http://pypi.python.org/pypi/django-paypal/0.4.1'},
 	{'name':'cmsplugin-twitter', 'url': 'http://pypi.python.org/pypi/cmsplugin-twitter/1.1.2'},
