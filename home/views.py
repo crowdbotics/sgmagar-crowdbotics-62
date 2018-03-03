3769bf9784de93a8eaf893236d4025b948eab2e2
Saurav <sp.gharti@gmail.com>
2018-03-03 18:50:58 +0000

    add package cmsplugin-twitter

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'cmsplugin-twitter', 'url': 'http://pypi.python.org/pypi/cmsplugin-twitter/1.1.2'},
     ]
     context = {
         'title': 'sgmagar-crowdbotics-62',
